d152f8fdbed30fbd04257a4a4397968aea1d3a4e
Last Phantom <paritosh191200@gmail.com>
2021-02-04 23:48:05 +0530

    tabu completed, beam bug removed

--- Lab3/Lab 3 solution/Lab 3 solution/Lab_3_solution.py
+++ Lab3/Lab 3 solution/Lab 3 solution/Lab_3_solution.py
@@ -105,7 +105,7 @@ def beamSearch(formula,n,beamLength):
     pq=PriorityQueue()
     pq.put((heuristic(formula,state),state))
     count=0
-    while(pq):
+    while not pq.empty():
         x=pq.get()
         state=x[1]
         count+=1
@@ -123,12 +123,59 @@ def beamSearch(formula,n,beamLength):
                     visited.append(nextState)
     print("Total states explored:",count)
 
+def tabuMoveGen(state):
+    neighbours = []
+    for i in range(len(state)):
+        neighbour = copy.deepcopy(state)
+        neighbour[i] = 1 - neighbour[i]
+        neighbours.append(neighbour)
+    return neighbours
 
-
-
+def tabuSearch(formula,n,tt=2):
+    print("\nStarting Tabu Search ...")
+    bestState=generateRandomState(n)
+    print("Initial State:",bestState)
+    bestCandidate = bestState
+    visitedStates = []
+    visitedStates.append(bestCandidate)
+    tabuList = [0]*n
+    flag = True
+    while(not goalTest(formula,bestState) and flag):
+        flag = False
+        print("Current tabu list : ",tabuList)
+        print("Prev bestcandidate : ",bestCandidate)
+        neighbours = tabuMoveGen(bestCandidate)
+        bestCandidate = []
+        tabuElement = -1
+        print("Neighbours : ",neighbours)
+        for i in range(len(tabuList)):
+            if tabuList[i]==0 :
+                if(neighbours[i] not in visitedStates):
+                    flag = True
+                    if len(bestCandidate) == 0:
+                        bestCandidate = neighbours[i]
+                        tabuElement = i
+                    else:
+                        if(heuristic(formula,neighbours[i]) > heuristic(formula,bestCandidate)) :
+                            bestCandidate = neighbours[i]
+                            tabuElement = i
+            else : 
+                tabuList[i] = tabuList[i]-1
+        visitedStates.append(bestCandidate)
+        tabuList[tabuElement] = tt
+        print("New bestcandidate : ",bestCandidate)
+        print("New Tabu list : ",tabuList)
+        if(heuristic(formula,bestCandidate) > heuristic(formula,bestState)):
+            bestState = bestCandidate
+            print("Best State updated, heuristic value = ",heuristic(formula,bestState))
+    if(flag == False):
+        print("No more states to explore!\n The best State so far is ",bestState)
+    else :
+        print("Best State is : ",bestState)
 
 def main():
-    f=generate_formula(4,5)
+    n=4
+    f=generate_formula(n,5)
     #f=[[1,2,3],[-1,2,3],[-2,1,3],[-3,1,2],[-2,-1,3],[-3,-1,2],[-3,-2,1],[-3,-2,-1]]
     #print(f)
     print_formula(f)
@@ -137,7 +184,9 @@ def main():
     #print(list(combinations(range(4),3)))
     #print(moveGen([1,1,0,0],1))
     #print(generateRandomState(4))
-    VND(f,4)
-    beamSearch(f,4,2)
+    VND(f,n)
+    beamSearch(f,n,2)
+    print("\n\n-- beam over | tabu begin --")
+    tabuSearch(f,n,2)
 
 main()
